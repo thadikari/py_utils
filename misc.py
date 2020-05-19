659d70150f8a238d87da35f7f8e7145bc5403b3d
Tharindu Adikari <5517829+thadikari@users.noreply.github.com>
2020-05-19 03:20:10 +0000

    impl logging

--- misc.py
+++ misc.py
@@ -1,4 +1,5 @@
 from collections import OrderedDict
+import logging
 
 
 class Registry:
@@ -14,3 +15,17 @@ class Registry:
     def reg(self, tp):
         self.put(tp.__name__, tp)
         return tp
+
+
+def setup_logger(logger=None, name='', log_level=logging.INFO): # INFO DEBUG
+    if not logger: logger = logging.getLogger(name)
+    if logger.hasHandlers(): logger.propagate = 0
+    logger.setLevel(log_level)
+    ch = logging.StreamHandler()
+    formatter = logging.Formatter(
+            '%(asctime)s---------------------|%(name)s|%(message)s',
+            # logging.BASIC_FORMAT,
+            "%H:%M:%S")
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+    return logger
