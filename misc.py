453b009dd00b316d23e57ad3a8666df14303970b
Tharindu Adikari <5517829+thadikari@users.noreply.github.com>
2022-12-23 16:03:38 -0500

    minor changes

--- misc.py
+++ misc.py
@@ -1,31 +1,39 @@
-from collections import OrderedDict
 import logging
+from collections import OrderedDict
 
 
 class Registry:
     def __init__(self): self.dd = OrderedDict()
+
     def keys(self): return list(self.dd.keys())
+
     def values(self): return list(self.dd.values())
+
     def items(self): return self.dd.items()
+
     def get(self, key, default=None): return self.dd.get(key, default)
+
+    def __getitem__(self, key): return self.dd[key]
+
     def put(self, key, val):
         # print(self.keys())
-        assert(key not in self.dd)
+        assert (key not in self.dd)
         self.dd[key] = val
+
     def reg(self, tp):
         self.put(tp.__name__, tp)
         return tp
 
 
-def setup_logger(logger=None, name='', log_level=logging.INFO): # INFO DEBUG
+def setup_logger(logger=None, name='', log_level=logging.INFO):  # INFO DEBUG
     if not logger: logger = logging.getLogger(name)
     if logger.hasHandlers(): logger.propagate = 0
     logger.setLevel(log_level)
     ch = logging.StreamHandler()
     formatter = logging.Formatter(
-            '%(asctime)s|%(name)s|%(message)s',
-            # logging.BASIC_FORMAT,
-            "%H:%M:%S")
+        '%(asctime)s|%(name)s|%(message)s',
+        # logging.BASIC_FORMAT,
+        "%H:%M:%S")
     ch.setFormatter(formatter)
     logger.addHandler(ch)
     return logger
