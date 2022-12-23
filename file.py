4b1a47ff1def1c634cf5ca783435c6a3baafccfe
Tharindu Adikari <5517829+thadikari@users.noreply.github.com>
2022-12-22 21:23:57 -0500

    impl common plotting functions

--- file.py
+++ file.py
@@ -1,18 +1,18 @@
-from collections import OrderedDict
-from itertools import zip_longest
 import argparse
 import csv
 import os
 import re
-
+from collections import OrderedDict
 
 pjoin, pabsl = os.path.join, os.path.abspath
+
 
 def resolve_data_dir(proj_name):
     return pabsl(pjoin(os.path.expanduser('~'), 'scratch', proj_name))
 
+
 def resolve_data_dir_os(proj_name, extra=[], create_dir=False):
-    if os.name == 'nt': # if windows
+    if os.name == 'nt':  # if windows
         curr_path = os.path.dirname(os.path.realpath(__file__))
         ret = pjoin(curr_path, '..', '..', 'data', *extra)
     else:
@@ -34,15 +34,18 @@ class CSVFile:
         self.csv.writerow(line)
         if flush: self.flush()
 
-    def flush(self): self.fp.flush()
+    def flush(self):
+        self.fp.flush()
 
     def __del__(self):
         self.fp.flush()
         self.fp.close()
 
+
 def make_csv(name, dir, header):
     if not os.path.exists(dir): os.makedirs(dir)
     return CSVFile(name, dir, header=header)
+
 
 def gen_unique_labels(long_names, tokens=['_', '__']):
     def inner(strings, token):
@@ -52,64 +55,77 @@ def gen_unique_labels(long_names, tokens=['_', '__']):
         common.intersection_update(*sets)
         return ['_'.join(it for it in spl if it not in common) for spl in spls]
 
-    if len(long_names)>1:
+    if len(long_names) > 1:
         for tk in tokens: long_names = inner(long_names, tk)
         return long_names
     else:
         return ['plot']
 
+
 def filter_strings(_a, dirs, log_else=True):
-    kw_filter = lambda nl_,f_,kwl: [n_ for n_ in nl_ if f_(kw in n_ for kw in kwl)]
+    kw_filter = lambda nl_, f_, kwl: [n_ for n_ in nl_ if f_(kw in n_ for kw in kwl)]
     if _a.not_kw: dirs = [dir for dir in dirs if all(kw not in dir for kw in _a.not_kw)]
     if _a.and_kw: dirs = kw_filter(dirs, all, _a.and_kw)
     if _a.or_kw: dirs = kw_filter(dirs, any, _a.or_kw)
-    if not dirs: print(f'No matches for', 'NOT:', _a.not_kw, ', AND:', _a.and_kw, ', OR:', _a.or_kw)
-    elif log_else: print('Collected:', *dirs, sep='\n')
+    if not dirs:
+        print(f'No matches for', 'NOT:', _a.not_kw, ', AND:', _a.and_kw, ', OR:', _a.or_kw)
+    elif log_else:
+        print('Collected:', *dirs, sep='\n')
     return dirs
+
 
 def filter_directories(_a, data_dir):
     if not os.path.isdir(data_dir):
         print(f'Not a directory: {data_dir}')
         return []
     dirs = filter_strings(_a, next(os.walk(data_dir))[1], log_else=False)
-    if not dirs: print(f'No matching directories found in {data_dir}.')
-    else: print('Collected %d directories.'%len(dirs))
+    if not dirs:
+        print(f'No matching directories found in {data_dir}.')
+    else:
+        print('Collected %d directories.' % len(dirs))
     return dirs
+
 
 # https://stackoverflow.com/questions/5967500/how-to-correctly-sort-a-string-with-a-number-inside
 def naturalkey(text):
     atoi = lambda tt: int(tt) if tt.isdigit() else tt
     return [atoi(c) for c in re.split(r'(\d+)', text)]
 
+
 def reorder(_a, bundles, keyf, zorderf=None):
     bundles.sort(key=lambda it: naturalkey(keyf(it)))
     if _a.reverse: bundles.reverse()
     if not zorderf is None: bundles.sort(key=zorderf)
-    strings = ['%s >> [%s]'%(bun,keyf(bun)) for bun in bundles]
+    strings = ['%s >> [%s]' % (bun, keyf(bun)) for bun in bundles]
     print(*strings, sep='\n')
+
 
 def bind_filter_args(parser):
     parser.add_argument('--and_kw', help='AND filter: allows only if all present', default=[], type=str, nargs='*')
     parser.add_argument('--or_kw', help='OR filter: allows if any is present', default=[], type=str, nargs='*')
-    parser.add_argument('--not_kw', help='NOT filter: allows only if these are NOT present', default=[], type=str, nargs='*')
+    parser.add_argument('--not_kw', help='NOT filter: allows only if these are NOT present', default=[], type=str,
+                        nargs='*')
+
 
 def bind_reorder_args(parser):
     # parser.add_argument('--natsort', help='sort by natural order of labels', action='store_true')
     parser.add_argument('--reverse', help='reverse after sorting', action='store_true')
     parser.add_argument('--property', action=PropertyAction, nargs='+', default={})
 
+
 class PropertyAction(argparse.Action):
     def __call__(self, parser, args, values, option_string=None):
         l_ = len(values)
-        if not (2<=l_<=3):
-            msg=f'argument {self.dest} accepts only 2-4 arguments: name, label, [fmt]'
+        if not (2 <= l_ <= 3):
+            msg = f'argument {self.dest} accepts only 2-4 arguments: name, label, [fmt]'
             raise parser.error(msg)
-        ll = [None]*3
+        ll = [None] * 3
         ll[:len(values)] = values
         val_dict = getattr(args, self.dest)
         if val_dict is None: val_dict = OrderedDict()
         val_dict[ll[0]] = ll[1:]
         setattr(args, self.dest, val_dict)
+
 
 class BundleBase:
     def __init__(self, _a, dir, label):
@@ -121,12 +137,25 @@ class BundleBase:
 
     def __str__(self): return self.dir
 
+
 def load_dirs(_a, data_dir, fac):
     dirs = filter_directories(_a, _a.data_dir)
     if not dirs: exit()
 
     labels = gen_unique_labels(dirs)
     bundles = [fac(_a, *args) for args in zip(dirs, labels)]
-    reorder(_a, bundles, lambda it: it.label, lambda it: it.zorder)
+    reorder(_a, bundles, lambda it: it.label, lambda it: it.z_order)
     for root in bundles: root.load()
     return bundles
+
+
+def csv2dict(file_path):
+    import pandas
+    try:
+        if os.stat(file_path).st_size == 0:
+            return {}
+        ds = pandas.read_csv(file_path, header=0).to_dict('Series')
+        return {key: ds[key].to_numpy() for key in ds}
+    except Exception as e:
+        print(f'Error reading: {file_path}.', e)
+        return {}
--- mpl.py
+++ mpl.py
@@ -1,14 +1,17 @@
-import matplotlib.pyplot as plt
-from cycler import cycler
-import numpy as np
-import matplotlib
 import argparse
+import os
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+from cycler import cycler
 
 
 def bind_init_args(parser):
     parser.add_argument('--font_size', default=20, type=int)
     parser.add_argument('--legend_font_size', default=18, type=int)
     parser.add_argument('--tick_size', default=16, type=int)
+
 
 def set_math_font(remove_type3=False):
     matplotlib.rcParams['mathtext.fontset'] = 'stix'
@@ -20,23 +23,27 @@ def set_math_font(remove_type3=False):
         matplotlib.rcParams['pdf.fonttype'] = 42
         matplotlib.rcParams['ps.fonttype'] = 42
 
-#https://stackoverflow.com/questions/11367736/matplotlib-consistent-font-using-latex
+
+# https://stackoverflow.com/questions/11367736/matplotlib-consistent-font-using-latex
 def init(font_size=None, legend_font_size=None, modify_cycler=True, tick_size=None, args=None):
+    if args.backend is not None:
+        matplotlib.use(args.backend)
     if args:
         font_size = args.font_size
         legend_font_size = args.legend_font_size
         tick_size = args.tick_size
-    custom_cycler = (cycler(color=['r', 'b', 'g', 'y', 'k', 'm', 'c']*4) +
-                     cycler(linestyle=['-', '--', ':', '-.']*7))
+    custom_cycler = (cycler(color=['r', 'b', 'g', 'y', 'k', 'm', 'c'] * 4) +
+                     cycler(linestyle=['-', '--', ':', '-.'] * 7))
     if modify_cycler: plt.rc('axes', prop_cycle=custom_cycler)
     set_math_font()
     if font_size: matplotlib.rcParams.update({'font.size': font_size})
-    if legend_font_size: plt.rc('legend', fontsize=legend_font_size)    # legend fontsize
+    if legend_font_size: plt.rc('legend', fontsize=legend_font_size)  # legend fontsize
     # https://stackoverflow.com/questions/3899980/how-to-change-the-font-size-on-a-matplotlib-plot
     if tick_size:
         matplotlib.rcParams['xtick.labelsize'] = tick_size
         matplotlib.rcParams['ytick.labelsize'] = tick_size
     # https://stackoverflow.com/questions/6390393/matplotlib-make-tick-labels-font-size-smaller
+
 
 def fmt_ax(ax, xlab, ylab, leg, grid=1, grid_kwargs={}):
     if leg: ax.legend(loc='best')
@@ -48,9 +55,10 @@ def fmt_ax(ax, xlab, ylab, leg, grid=1, grid_kwargs={}):
 
 # https://stackoverflow.com/questions/51323505/how-to-make-relim-and-autoscale-in-a-scatter-plot
 def update_datalim(ax, scatter):
-    #ax.ignore_existing_data_limits = True
+    # ax.ignore_existing_data_limits = True
     ax.update_datalim(scatter.get_datalim(ax.transData))
     ax.autoscale_view()
+
 
 def get_data_setter(ax, obj):
     if isinstance(obj, matplotlib.collections.PathCollection):
@@ -59,32 +67,38 @@ def get_data_setter(ax, obj):
     elif isinstance(obj, matplotlib.lines.Line2D):
         setter = lambda xv, yv: (obj.set_xdata(xv), obj.set_ydata(yv))
         return (obj.get_xdata(), obj.get_ydata()), setter
-    else: return None
+    else:
+        return None
+
 
 def relim_axis(ax, xl=None, xu=None, yl=None, yu=None):
     lim = lambda idx, xv_, yv_: (xv_[idx], yv_[idx])
     ax.ignore_existing_data_limits = True
-    for obj in ax.get_children(): # get_lines also works if line plot
+    for obj in ax.get_children():  # get_lines also works if line plot
         ret = get_data_setter(ax, obj)
         if ret is None: continue
         (xv, yv), setter = ret
-        if not xl is None: xv, yv = lim(xv>=xl, xv, yv)
-        if not xu is None: xv, yv = lim(xv<=xu, xv, yv)
-        if not yl is None: xv, yv = lim(yv>=yl, xv, yv)
-        if not yu is None: xv, yv = lim(yv<=yu, xv, yv)
+        if not xl is None: xv, yv = lim(xv >= xl, xv, yv)
+        if not xu is None: xv, yv = lim(xv <= xu, xv, yv)
+        if not yl is None: xv, yv = lim(yv >= yl, xv, yv)
+        if not yu is None: xv, yv = lim(yv <= yu, xv, yv)
         setter(xv, yv)
         ax.ignore_existing_data_limits = False
     ax.relim(), ax.autoscale_view()
+
 
 def save_show_fig(args, plt, file_path, tight_layout=True, transparent=False):
     if tight_layout: plt.tight_layout()
     if args.save:
         ext_str = ','.join(args.ext)
-        if len(args.ext)>1: ext_str = f'{{{ext_str}}}'
+        if len(args.ext) > 1: ext_str = f'{{{ext_str}}}'
         print(f'Saving figure to {file_path}.{ext_str}')
         for ext in args.ext:
-            plt.savefig('%s.%s'%(file_path,ext), bbox_inches='tight', pad_inches=args.pad_inches, transparent=transparent)
-    else: plt.show()
+            plt.savefig('%s.%s' % (file_path, ext), bbox_inches='tight', pad_inches=args.pad_inches,
+                        transparent=transparent)
+    else:
+        plt.show()
+
 
 def set_ax_props(args, ax):
     if args.hide_ylabel: ax.set_ylabel(None)
@@ -95,7 +109,9 @@ def set_ax_props(args, ax):
     if not args.ymin is None: ax.set_ylim(bottom=args.ymin)
     if not args.ymax is None: ax.set_ylim(top=args.ymax)
 
+
 def bind_plotting_args(parser):
+    # general plotting args
     parser.add_argument('--xmax', type=float)
     parser.add_argument('--xmin', type=float)
     parser.add_argument('--ymin', type=float)
@@ -105,76 +121,153 @@ def bind_plotting_args(parser):
     parser.add_argument('--hidex', action='store_true')
     parser.add_argument('--hide_ylabel', action='store_true')
     parser.add_argument('--nolegend', action='store_true')
-    parser.add_argument('--ax_size', help='width, height {scale} per axis', type=float, nargs='+', action=AxSizeAction, default=[8,5])
+    parser.add_argument('--backend', choices=matplotlib.rcsetup.all_backends, default=None)
+    parser.add_argument('--ax_size', help='width, height {scale} per axis', type=float, nargs='+', action=AxSizeAction,
+                        default=[8, 5])
+
+    # plot_improvement args
     parser.add_argument('--imprv_percentage', type=float, nargs=4, action='append')
     parser.add_argument('--font_size_imprv', type=int, default=20)
-    parser.add_argument('--title', type=str)
+
+    # plot_improvement args
+    parser.add_argument('--plot_separate', help='plot axes on grid or separately', action='store_true')
+    parser.add_argument('--share_x', action='store_true')
+    parser.add_argument('--legend_ax', type=int, default=-1)
+    parser.add_argument('--rows_cols', nargs=2, type=int, default=None)
+    parser.add_argument('--hspace', help='hspace in subplots_adjust', type=float, default=0)
+
 
 def plot_improvement(args, ax):
     # plot vertical double arrow lines with an percentage annotation
     if not args.imprv_percentage is None:
-        for xval,bot,top,sgn in args.imprv_percentage:
-            imprv = int(100*(top-bot)/top)
+        for xval, bot, top, sgn in args.imprv_percentage:
+            imprv = int(100 * (top - bot) / top)
             ax.annotate(s='', xy=(xval, bot), xytext=(xval, top), arrowprops=dict(arrowstyle='<->'))
-            ha = 'right' if sgn<0 else 'left'
-            ax.annotate(f'{imprv}%', xy=(xval+np.sign(sgn)*0.2, (top+bot)/2), ha=ha, va='center', size=args.font_size_imprv)
+            ha = 'right' if sgn < 0 else 'left'
+            ax.annotate(f'{imprv}%', xy=(xval + np.sign(sgn) * 0.2, (top + bot) / 2), ha=ha, va='center',
+                        size=args.font_size_imprv)
+
 
 def bind_fig_save_args(parser):
     parser.add_argument('--save', help='save plots', action='store_true')
     exts_ = ['png', 'pdf']
     parser.add_argument('--ext', help='plot save extention', nargs='*', default=exts_, choices=exts_)
-    parser.add_argument('--pad_inches', type=float, default=None) # good choice is 0.03
+    parser.add_argument('--pad_inches', type=float, default=None)  # good choice is 0.03
+
 
 def get_subplot_config(count):
-    return {1:(1,1), 2:(1,2), 3:(1,3), 4:(2,2),
-            5:(2,3), 6:(2,3),
-            7:(3,3), 8:(3,3), 9:(3,3),
-            10:(3,4), 11:(3,4), 12:(3,4),
-            13:(4,4), 14:(4,4), 15:(4,4), 16:(4,4),
-            17:(4,5), 18:(4,5), 19:(4,5), 20:(4,5),
-            21:(4,6), 22:(4,6), 23:(4,6), 24:(4,6),
-           }[count]
+    return {1: (1, 1), 2: (1, 2), 3: (1, 3), 4: (2, 2),
+            5: (2, 3), 6: (2, 3),
+            7: (3, 3), 8: (3, 3), 9: (3, 3),
+            10: (3, 4), 11: (3, 4), 12: (3, 4),
+            13: (4, 4), 14: (4, 4), 15: (4, 4), 16: (4, 4),
+            17: (4, 5), 18: (4, 5), 19: (4, 5), 20: (4, 5),
+            21: (4, 6), 22: (4, 6), 23: (4, 6), 24: (4, 6),
+            }[count]
+
 
 def get_subplot_axes(ax_size, count=None, rows_cols=None, subplots_kwargs=None):
     # ax_size = [width, height] per axis
     # if fig is None: fig = plt.gcf()
-    if count is None and rows_cols is None: raise('Incomplete arguments')
-    if count is None: count = rows_cols[0]*rows_cols[1]
-    if rows_cols is None or rows_cols[0]*rows_cols[1]<count:
+    if count is None and rows_cols is None: raise ('Incomplete arguments')
+    if count is None: count = rows_cols[0] * rows_cols[1]
+    if rows_cols is None or rows_cols[0] * rows_cols[1] < count:
         rows_cols = get_subplot_config(count)
     if subplots_kwargs is None: subplots_kwargs = {}
     fig, axes = plt.subplots(*rows_cols, **subplots_kwargs)
-    fig.set_size_inches(ax_size[0]*rows_cols[1], ax_size[1]*rows_cols[0])
+    fig.set_size_inches(ax_size[0] * rows_cols[1], ax_size[1] * rows_cols[0])
     # axes = [fig.add_subplot(*rows_cols,idx+1) for idx in range(count)]
     axes = axes.flatten() if isinstance(axes, np.ndarray) else [axes]
     return axes[:count], fig
 
-def set_best_time_scale(ax, seconds, label=''):
-    if seconds>3600*24 *5: div,unit,steps = 3600*24, 'days', [8.64]
-    elif seconds>3600 *5: div,unit,steps = 3600, 'hrs', [9]
-    elif seconds>60 *5: div,unit,steps = 60, 'min', [3,6,9]
-    elif seconds>1 *1: div,unit,steps = 1, 's', [1,2,3,4,5,10]
-    else: div,unit,steps = 1e-3, 'ms', [1,2,4,5,10]
 
-    fmt = plt.FuncFormatter(lambda x,pos:'%g'%(x/div))
+def set_best_time_scale(ax, seconds, label=''):
+    if seconds > 3600 * 24 * 5:
+        div, unit, steps = 3600 * 24, 'days', [8.64]
+    elif seconds > 3600 * 5:
+        div, unit, steps = 3600, 'hrs', [9]
+    elif seconds > 60 * 5:
+        div, unit, steps = 60, 'min', [3, 6, 9]
+    elif seconds > 1 * 1:
+        div, unit, steps = 1, 's', [1, 2, 3, 4, 5, 10]
+    else:
+        div, unit, steps = 1e-3, 'ms', [1, 2, 4, 5, 10]
+
+    fmt = plt.FuncFormatter(lambda x, pos: '%g' % (x / div))
     loc = plt.MaxNLocator('auto', steps=steps)
     # hack to get rid of MaxNLocator adding 10 to steps, which results in ugly hour ticks
     # https://stackoverflow.com/questions/62148592/use-matplotlib-ticker-locator-to-put-ticks-only-if-location-is-divisible-by-a-gi
-    if unit=='hrs':
+    if unit == 'hrs':
         loc._extended_steps = loc._staircase(np.array([0.9, 1.8, 2.7, 3.6, 5.4, 7.2, 9]))
     ax.xaxis.set_major_formatter(fmt)
     ax.xaxis.set_major_locator(loc)
     return f'{label} ({unit})'
 
+
 # https://stackoverflow.com/questions/4194948/python-argparse-is-there-a-way-to-specify-a-range-in-nargs
 class AxSizeAction(argparse.Action):
     def __call__(self, parser, args, values, option_string=None):
         l_ = len(values)
-        if not (0<l_<4):
-            msg=f'argument {self.dest} accepts only 1 or 2 or 3 arguments'
+        if not (0 < l_ < 4):
+            msg = f'argument {self.dest} accepts only 1 or 2 or 3 arguments'
             raise parser.error(msg)
-        if l_==1: (v1,v2), mul = self.default, values[0]
-        if l_==2: (v1,v2), mul = values, 1.
-        if l_==3: v1,v2,mul = values
-        values = v1*mul,v2*mul
+        v1, v2, mul = 1, 1, 1
+        if l_ == 1: (v1, v2), mul = self.default, values[0]
+        if l_ == 2: (v1, v2), mul = values, 1.
+        if l_ == 3: v1, v2, mul = values
+        values = v1 * mul, v2 * mul
         setattr(args, self.dest, values)
+
+
+def plot_callables(args, callables, title):
+    if not args.plot_separate:
+        plot_on_grid(args, callables, title)
+    else:
+        plot_separate(args, callables)
+
+
+def plot_on_grid(args, callables, title):
+    """
+    Prep a grid of axes and call each item in `callable_list` to plot on the axis.
+    """
+    subplots_kwargs = {'share_x': 'col'} if args.share_x else {}
+    axes, fig = get_subplot_axes(args.ax_size, len(callables),
+                                 rows_cols=args.rows_cols,
+                                 subplots_kwargs=subplots_kwargs)
+    for ax, cb in zip(axes, callables): cb(ax)
+    for ind, ax in enumerate(axes):
+        if 0 <= args.legend_ax != ind:
+            leg = ax.get_legend()
+            if leg is not None:
+                leg.remove()
+        # set x label to only the bottom-most axes
+        # following hardcoded line need to be refactored
+        # if args.share_x and ind != 2: ax.set_xlabel(None)
+    fig.canvas.manager.set_window_title(f'{title}: {args.data_dir}')
+    tight = True
+    if args.hspace > 0:
+        plt.subplots_adjust(hspace=args.hspace)
+        tight = False
+    save_show_fig(args, plt, os.path.join(args.data_dir, title), tight_layout=tight)
+
+
+def plot_separate(args, callables):
+    """
+    Call each item in `callable_list` to plot on the axis while saving figures.
+    """
+    fig = plt.gcf()
+    for cb in callables:
+        fig.clf()
+        fig.set_size_inches(args.ax_size[0], args.ax_size[1])
+        cb(plt.gca())
+        title = cb.__name__
+        fig.canvas.manager.set_window_title(f'{title}: {args.data_dir}')
+        save_show_fig(args, plt, os.path.join(args.data_dir, title))
+
+
+def get_fmt(ax):
+    """
+    Get format of next item in prop_cycler and advance one position.
+    """
+    prop = next(ax._get_lines.prop_cycler)
+    return '%s%s' % (prop['color'], prop['linestyle'])
